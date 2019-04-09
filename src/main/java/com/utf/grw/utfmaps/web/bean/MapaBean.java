07fb1ec4173204514523832bda3625697000742a
Gabriellsl <gabriellsl2009@hotmail.com>
2019-04-09 12:04:43 -0300

    criacao da estrutua basica da tela inicial

---
+++ src/main/java/com/utf/grw/utfmaps/web/bean/MapaBean.java
@@ -0,0 +1,18 @@
+
+package com.utf.grw.utfmaps.web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.swing.JOptionPane;
+
+@ManagedBean(name = "mapaBean")
+@RequestScoped
+public class MapaBean {
+    
+    
+    public void teste(){
+        System.out.println("Testando...");
+        JOptionPane.showMessageDialog(null, "Eeeita !");
+    }
+    
+}
