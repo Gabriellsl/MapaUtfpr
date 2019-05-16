15cec044e3a10a479927971973178784f74ea65f
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-05-15 21:26:20 -0300

    update logger

--- src/main/java/com/utf/grw/utfmaps/util/Logger.java
+++ src/main/java/com/utf/grw/utfmaps/util/Logger.java
@@ -6,7 +6,6 @@
 package com.utf.grw.utfmaps.util;
 
 import com.utf.grw.utfmaps.modelo.logger.Log;
-import com.utf.grw.utfmaps.modelo.logger.LogDAOHibernate;
 import java.util.logging.Level;
 
 /**
@@ -15,26 +14,12 @@ import java.util.logging.Level;
  */
 
 public class Logger {
-    public static void persist(Class clazz, String error){
+    public static void save(Class clazz, String error){
         Log log = new Log(clazz, error);
-        LogDAOHibernate logDao = new LogDAOHibernate();
         try {
-            logDao.salvar(log);
+            //
         } catch (Exception ex) {
             java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    
-    public static void log(Class clazz, String error){
-        Log log = new Log(clazz, error);
-        System.err.print("Erro na classe "+clazz.getClass().getName()+"\n"+"Erro: "+error);
-    }
-    
-    public static void logAndPersist(Class clazz, String error){
-        Log log = new Log(clazz, error);
-        persist(clazz, error);
-        Logger.log(clazz, error);
-    }
-    
-    
+    } 
 }
