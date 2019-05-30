8ef6a016bb7635f6f78f1d2730203b9547497521
Gabriel Lenin <gabriellsl2009@hotmail.com>
2019-05-30 12:35:21 -0300

    Criacao tela de sugestoes

--- src/main/java/com/utf/grw/utfmaps/modelo/sugestao/Sugestao.java
+++ src/main/java/com/utf/grw/utfmaps/modelo/sugestao/Sugestao.java
@@ -3,23 +3,23 @@ package com.utf.grw.utfmaps.modelo.sugestao;
 import com.utf.grw.utfmaps.modelo.departamento.Departamento;
 
 public class Sugestao {
-    private Long idServidor;
+  //  private Long idServidor;
     
     private String sugestao;
     
-    private String nome;
+  //  private String nome;
     
-    private Departamento dep;
+   // private Departamento dep;
 
     public Sugestao() {
     }
     
     public Sugestao(String sugestao, String nome, Departamento dep) {
         this.sugestao = sugestao;
-        this.nome = nome;
-        this.dep = dep;
+    //    this.nome = nome;
+    //    this.dep = dep;
     }
-
+/*
     public Long getIdServidor() {
         return idServidor;
     }
@@ -27,7 +27,7 @@ public class Sugestao {
     public void setIdServidor(Long idServidor) {
         this.idServidor = idServidor;
     }
-
+*/
     public String getSugestao() {
         return sugestao;
     }
@@ -35,7 +35,7 @@ public class Sugestao {
     public void setSugestao(String sugestao) {
         this.sugestao = sugestao;
     }
-
+/*
     public String getNome() {
         return nome;
     }
@@ -51,5 +51,5 @@ public class Sugestao {
     public void setDep(Departamento dep) {
         this.dep = dep;
     }
-    
+  */  
 }
